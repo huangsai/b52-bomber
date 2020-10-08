7b0a6042afb853dec0f429880bed187e62231b1f
df <jj866338@gmail.com>
2020-10-08 17:54:28 +0800

    细节修改、消息页面 通知加入圆点。

--- b52-bomber/feature-tik/src/main/java/com/mobile/app/bomber/tik/message/items/MsgItem.java
+++ b52-bomber/feature-tik/src/main/java/com/mobile/app/bomber/tik/message/items/MsgItem.java
@@ -25,7 +25,14 @@ public class MsgItem extends SimpleRecyclerItem {
         // 绑定图片加载，逻辑在MsgFragment.load(...)方法
         // MsgFragment.onCreate(...)方法设置了图片加载器
         // MsgFragment实现AdapterImageLoader接口
-//        holder.attachImageLoader(R.id.img_profile);
+        binding.txtTitle.setText("系统通知");
+        binding.imgProfile.setImageResource(R.drawable.default_profile);
+        binding.txtContent.setText("动漫合计，不容错过");
+        binding.txtTime.setText("2020-08-28");
+
+
+        holder.attachImageLoader(R.id.img_profile);
+        holder.attachOnClickListener(R.id.item_msg);
     }
 
     @Override
