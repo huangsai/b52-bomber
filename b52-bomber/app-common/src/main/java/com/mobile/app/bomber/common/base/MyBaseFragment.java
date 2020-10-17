30db6966b222e2e38f3dc7a9c6075f15665cb63a
barry <barryme2049@gmail.com>
2020-10-17 17:32:44 +0800

    新maven

--- b52-bomber/app-common/src/main/java/com/mobile/app/bomber/common/base/MyBaseFragment.java
+++ b52-bomber/app-common/src/main/java/com/mobile/app/bomber/common/base/MyBaseFragment.java
@@ -11,7 +11,6 @@ import org.jetbrains.annotations.Nullable;
 
 public abstract class MyBaseFragment extends BaseFragment {
 
-    private int _onResumeCount = 0;
     protected int position = -1;
 
     public MyBaseFragment() {
@@ -29,16 +28,6 @@ public abstract class MyBaseFragment extends BaseFragment {
         if (args != null) {
             position = args.getInt("position", -1);
         }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        _onResumeCount++;
-    }
-
-    public int getOnResumeCount() {
-        return _onResumeCount;
     }
 
     public int getPosition() {
