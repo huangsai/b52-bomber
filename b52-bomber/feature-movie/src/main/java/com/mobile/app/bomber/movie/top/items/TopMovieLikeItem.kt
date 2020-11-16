554e42f1cd7f2c2da06f142bfba348157ca54005
zen <jj866338@gmail.com>
2020-11-16 14:37:32 +0800

    判空处理

--- b52-bomber/feature-movie/src/main/java/com/mobile/app/bomber/movie/top/items/TopMovieLikeItem.kt
+++ b52-bomber/feature-movie/src/main/java/com/mobile/app/bomber/movie/top/items/TopMovieLikeItem.kt
@@ -12,6 +12,12 @@ class TopMovieLikeItem(val data: ApiMovie.Movie) : SimpleRecyclerItem() {
     override fun bind(holder: AdapterViewHolder) {
         val binding = holder.binding(MovieItemTopVerBinding::bind)
         binding.txtLabel.text = data.name
+        if (data.desc.isNullOrEmpty()) {
+            binding.txtDesc.text = "视频简介 暂无"
+        } else {
+            binding.txtDesc.text = data.desc
+
+        }
         binding.txtDesc.text = data.desc
         holder.attachOnClickListener(R.id.item_top_movie_ver)
         holder.attachImageLoader(R.id.img_cover)
