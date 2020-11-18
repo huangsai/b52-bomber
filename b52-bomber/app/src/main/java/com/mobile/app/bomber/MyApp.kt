c082772ec295103b7fde4633c764edd9bfd7a2b2
Jordan <Jordan>
2020-11-18 18:15:20 +0800

    崩溃收集器

--- b52-bomber/app/src/main/java/com/mobile/app/bomber/MyApp.kt
+++ b52-bomber/app/src/main/java/com/mobile/app/bomber/MyApp.kt
@@ -1,7 +1,11 @@
 package com.mobile.app.bomber
 
+import android.util.Log
 import androidx.multidex.MultiDexApplication
+import com.flurry.android.FlurryAgent
+import com.flurry.android.FlurryPerformance
 import com.mobile.app.bomber.runner.RunnerX
+
 
 class MyApp : MultiDexApplication() {
 
@@ -9,5 +13,15 @@ class MyApp : MultiDexApplication() {
         super.onCreate()
         RunnerX.setup(this, BuildConfig.DEBUG)
 //        Ipv6X.setup(this,true)
+        FlurryAgent.Builder()
+                .withLogEnabled(true)
+                .build(this, "BM6DYJYMWNFT6TNJCPMB");
+//        FlurryAgent.Builder()
+//                .withDataSaleOptOut(false) //CCPA - the default value is false
+//                .withCaptureUncaughtExceptions(true)
+//                .withIncludeBackgroundSessionsInMetrics(true)
+//                .withLogLevel(Log.VERBOSE)
+//                .withPerformanceMetrics(FlurryPerformance.ALL)
+//                .build(this, FLURRY_API_KEY)
     }
 }
